9ca3be01ecc2f8b22d64353c5317fb86620dfc41
Tom <tm731531@hotmail.com>
2021-03-06 12:27:17 +0800

    Init DB

---
+++ JKO.DB/dbo/Tables/jko_listing.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[jko_listing] (
+    [id]          INT            IDENTITY (1, 1) NOT NULL,
+    [title]       NVARCHAR (100) NOT NULL,
+    [description] NVARCHAR (100) NOT NULL,
+    [price]       INT            NOT NULL,
+    [user_id]     INT            NOT NULL,
+    [create_time] DATETIME2 (7)  CONSTRAINT [DF_jko_listing_create_time] DEFAULT (getdate()) NOT NULL,
+    CONSTRAINT [PK_jko_listing] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
---
+++ JKO.DB/dbo/Tables/jko_user.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[jko_user] (
+    [id]        INT           IDENTITY (1, 1) NOT NULL,
+    [user_name] VARCHAR (100) NOT NULL,
+    CONSTRAINT [PK_jko_user] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [u_jko_user_user_name]
+    ON [dbo].[jko_user]([user_name] ASC);
+
